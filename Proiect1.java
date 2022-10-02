4ab329739677c79a1635ed789bc9a4fed7f98a12
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2022-10-02 13:24:41 +0300

    Update Proiect1.java

--- Proiect1.java
+++ Proiect1.java
@@ -13,6 +13,12 @@ class Proiect1 {
         Random random = new Random();
         String title, author, genre;
         int pages;
+        char sortOpt = '\0', searchOpt = '\0';
+        int chosenBook = -1;
+        int pagesToRead = -1;
+        String searchFor = "";
+        boolean found = false;
+        Book temp = new Book("", "", "", 0);
 
         for (i = 0; i < books.length; i++) {
             System.out.println("Give the title, author, genre and number of pages of your book");
@@ -26,18 +32,15 @@ class Proiect1 {
         }
         System.out.println();
 
-        Book temp = new Book("", "", "", 0);
-        char sortOpt = '\0';
-        int chosenBook = -1;
-        int pagesToRead = -1;
-
         while (option != 0) {
             System.out.println(
-                    "Choose an option:\n\t1. Add a new book\n\t2. Sort your books\n\t3. Read something\n\t0. Exit");
+                    "Choose an option:\n\t1. Add a new book\n\t2. Read something\n\t3. Sort your books\n\t4. Search for a book\n\t0. Exit");
             option = scanner.nextInt();
             scanner.nextLine();
             switch (option) {
                 case 1:
+                    // se instantiaza un nou obiect de tip Book
+                    // cu metodele set se seteaza proprietatile noului obiect
                     Book newBook = new Book("", "", "", 0);
                     System.out.print("What's your new book's title? ");
                     title = scanner.nextLine();
@@ -56,7 +59,7 @@ class Proiect1 {
                     // se copiaza elementele in noul array
                     for (i = 0; i < books.length; i++)
                         newBooks[i] = books[i];
-                    // se adauga noul element
+                    // se adauga noul element la ultimul index
                     newBooks[numOfBooks] = newBook;
                     // se copiaza in arrayul initial
                     books = newBooks;
@@ -64,10 +67,32 @@ class Proiect1 {
                         System.out.println(books[i].toString());
                     break;
                 case 2:
+                    System.out.println("Which book would you like to read?");
+                    for (i = 0; i < books.length; i++) {
+                        System.out.println("\t" + (i + 1) + ". " + books[i].getTitle() + " by " + books[i].getAuthor());
+                    }
+                    chosenBook = scanner.nextInt();
+                    pagesToRead = random.nextInt(100);
+                    // se verifica daca este posibila citirea atator pagini cate au fost generate
+                    // random
+                    if (!books[chosenBook - 1].read(pagesToRead)) {
+                        System.out.println("You want to read " + pagesToRead + " pages, but you only have "
+                                + (books[chosenBook - 1].getPages() - books[chosenBook - 1].getPagesRead())
+                                + " pages left!");
+                    } else if (books[chosenBook - 1].getPages() == books[chosenBook - 1].getPagesRead()) {
+                        System.out.println("You finished " + books[chosenBook - 1].getTitle());
+                    } else {
+                        System.out.println(
+                                "You have " + (books[chosenBook - 1].getPages() - books[chosenBook - 1].getPagesRead())
+                                        + " pages left of " + books[chosenBook - 1].getTitle());
+                    }
+                    break;
+                case 3:
                     while (sortOpt != 'x') {
                         System.out.println(
-                                "Choose an option:\n\ta. Sort aphabetically by title\n\tb. Sort aphabetically by author\n\tc. Sort by number of pages left to read\n\tx. Exit");
+                                "Choose an option:\n\ta. Sort alphabetically by title\n\tb. Sort alphabetically by author\n\tc. Sort by number of pages left to read\n\tx. Exit");
                         sortOpt = scanner.next().charAt(0);
+                        // sortare prin selectie directa in functie de optiunea selectata
                         switch (sortOpt) {
                             case 'a':
                                 for (i = 0; i < books.length; i++) {
@@ -120,23 +145,47 @@ class Proiect1 {
                         }
                     }
                     break;
-                case 3:
-                    System.out.println("Which book would you like to read?");
-                    for (i = 0; i < books.length; i++) {
-                        System.out.println("\t" + (i + 1) + ". " + books[i].getTitle() + " by " + books[i].getAuthor());
-                    }
-                    chosenBook = scanner.nextInt();
-                    pagesToRead = random.nextInt(100);
-                    if (!books[chosenBook - 1].read(pagesToRead)) {
-                        System.out.println("You want to read " + pagesToRead + " pages, but you only have "
-                                + (books[chosenBook - 1].getPages() - books[chosenBook - 1].getPagesRead())
-                                + " pages left!");
-                    } else if (books[chosenBook - 1].getPages() == books[chosenBook - 1].getPagesRead()) {
-                        System.out.println("You finished " + books[chosenBook - 1].getTitle());
-                    } else {
-                        System.out.println(
-                                "You have " + (books[chosenBook - 1].getPages() - books[chosenBook - 1].getPagesRead())
-                                        + " pages left of " + books[chosenBook - 1].getTitle());
+                case 4:
+                    while (searchOpt != 'x') {
+                        found = false;
+                        System.out.println("Choose an option:\n\ta. Search by title\n\tb. Search by author\n\tx. Exit");
+                        searchOpt = scanner.next().charAt(0);
+                        scanner.nextLine();
+                        // cautare secventiala in functie de optiunea aleasa
+                        switch (searchOpt) {
+                            case 'a':
+                                System.out.println("Which book are you searching for?");
+                                searchFor = scanner.nextLine();
+                                for (i = 0; i < books.length; i++) {
+                                    if (books[i].getTitle().equals(searchFor)) {
+                                        System.out.println(searchFor + " was found!");
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (found == false) {
+                                    System.out.println("You don't have " + searchFor + " in your library!");
+                                }
+                                break;
+                            case 'b':
+                                System.out.println("Which author's books are you searching for?");
+                                searchFor = scanner.nextLine();
+                                for (i = 0; i < books.length; i++) {
+                                    if (books[i].getAuthor().equals(searchFor)) {
+                                        System.out.println(books[i].getTitle() + " by " + searchFor + " was found!");
+                                        found = true;
+                                    }
+                                }
+                                if (found == false) {
+                                    System.out
+                                            .println("You don't have anything from " + searchFor + " in your library!");
+                                }
+                                break;
+                            case 'x':
+                                break;
+                            default:
+                                System.out.println("Invalid option");
+                        }
                     }
                     break;
                 case 0:
@@ -146,5 +195,6 @@ class Proiect1 {
             }
             System.out.println();
         }
+        scanner.close();
     }
 }
